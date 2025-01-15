ffb59049706d8de5d15ab2903fe8dea891ba7623
biabbye <biabbye@gmail.com>
2025-01-15 14:57:12 +0200

    ESLint Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-
-
 export default function Home() {
   return (
-  <h1 className="text-3xl text-center text-violet-500 font-black">
-    Ultimate next js 15 course!!
-  </h1>
+    <h1 className="text-3xl text-center text-violet-500 font-black">
+      Ultimate next js 15 course!!
+    </h1>
   );
 }
