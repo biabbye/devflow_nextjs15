705a09e6caf5651bf42cd9680bd3bbf616c65478
biabbye <biabbye@gmail.com>
2025-01-16 14:06:51 +0200

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-center text-violet-500 font-black">
-      Ultimate next js 15 course!!
-    </h1>
+    <>
+      <h1 className="h1-bold">Ultimate next js 15 course!!</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome Inter</h1>
+    </>
   );
 }
